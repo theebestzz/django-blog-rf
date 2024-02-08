aa925c96a000ded191b15a97153fe06307c48a25
theebestzz <f.eminn18@gmail.com>
2024-02-08 12:52:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Udemy üzerinde vermiş olduğum **DJANGO REST FRAMEWORK EĞİTİM SETİ** eğitiminin kaynak kodlarıdır. Aşağıda ki link üzerinden eğitim setlerime ulaşabilirsiniz. 
+
+
+DJANGO REST FRAMEWORK EĞİTİM LİNKİ
+https://www.udemy.com/course/django-rest-framework-egitim-seti/?couponCode=DJANGORESTFRAMEWORK
+
+Django 3 | Profesyonel Web Programlama
+https://www.udemy.com/course/django-3-profesyonel-web-programlama/?couponCode=DJANGOPROFESYONEL
+
---
+++ post/api/views.py
@@ -0,0 +1,58 @@
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.generics import (ListAPIView,
+                                     RetrieveAPIView,
+                                     DestroyAPIView,
+                                     RetrieveUpdateAPIView,
+                                     CreateAPIView,)
+#Create model mixin
+from rest_framework.mixins import DestroyModelMixin
+
+from account.api.throttles import RegisterThrottle
+from post.api.paginations import PostPagination
+from post.api.permissions import IsOwner
+
+from post.api.serializers import PostSerializer, PostUpdateCreateSerializer
+from post.models import Post
+from rest_framework.permissions import (
+    IsAuthenticated,
+)
+
+
+class PostListAPIView(ListAPIView):
+    #throttle_scope  = 'hasan'
+    serializer_class = PostSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title','content']
+    pagination_class = PostPagination
+
+    def get_queryset(self):
+        queryset = Post.objects.filter(draft=False)
+        return queryset
+
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'slug'
+
+
+class PostUpdateAPIView(RetrieveUpdateAPIView, DestroyModelMixin):
+    queryset = Post.objects.all()
+    serializer_class = PostUpdateCreateSerializer
+    lookup_field = 'slug'
+    permission_classes = [IsOwner]
+
+    def perform_update(self, serializer):
+        serializer.save(modified_by = self.request.user)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class PostCreateAPIView(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostUpdateCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
+
